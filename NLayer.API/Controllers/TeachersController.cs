f8a9356368f0a02edd1d09bb69c6aecf86a42f79
Ferhat Cakmakoglu <ferhat.cakmakoglu61@gmail.com>
2023-03-26 15:59:11 +0300

    Teacher Controller Complated

---
+++ NLayer.API/Controllers/TeachersController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    public class TeachersController : BaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Teacher> _service;
+
+        public TeachersController(IMapper mapper, IService<Teacher> service)
+        {
+            _mapper = mapper;
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var teachers = await _service.GetAllAsync();
+            var teachersDto = _mapper.Map<List<TeacherDto>>(teachers.ToList());
+            return CreateActionResult(CustomResponseDto<List<TeacherDto>>.Succes(200, teachersDto));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var teacher = await _service.GetByIdAsync(id);
+            var teacherDto = _mapper.Map<TeacherDto>(teacher);
+            return CreateActionResult(CustomResponseDto<TeacherDto>.Succes(200, teacherDto));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(TeacherDto teacherDto)
+        {
+            var teacher = await _service.AddAsync(_mapper.Map<Teacher>(teacherDto));
+            var teachersDto = _mapper.Map<TeacherDto>(teacher);
+            return CreateActionResult(CustomResponseDto<TeacherDto>.Succes(201,teacherDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(TeacherDto teacherDto)
+        {
+            await _service.UpdateAsync(_mapper.Map<Teacher>(teacherDto));
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Succes(204));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var teacher = await _service.GetByIdAsync(id);
+            await _service.RemoveAsync(teacher);
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Succes(204));
+        }
+    }
+}
